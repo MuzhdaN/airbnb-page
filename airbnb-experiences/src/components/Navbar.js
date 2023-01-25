5e21ecfa36339fd9ded4235cac6c1ec62b8bfc7d
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-25 11:51:46 +0000

    Added navbar

---
+++ airbnb-experiences/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import logo from '../images/Vector.png'
+export default function Navbar() {
+    return(
+        <nav>
+            <img src={logo} />
+        </nav>
+    );
+}
