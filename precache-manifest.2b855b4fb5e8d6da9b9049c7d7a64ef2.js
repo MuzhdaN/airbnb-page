aa64193b1b11842330fc97e67e2861032316e9ef
MuzhdaN <muzhda.noorzad122@gmail.com>
2023-01-30 14:51:38 +0000

    Updates

---
+++ precache-manifest.2b855b4fb5e8d6da9b9049c7d7a64ef2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b08c522c631ad2347444",
+    "url": "/airbnb-page/static/css/main.cd01c670.chunk.css"
+  },
+  {
+    "revision": "b08c522c631ad2347444",
+    "url": "/airbnb-page/static/js/main.b08c522c.chunk.js"
+  },
+  {
+    "revision": "08e9374f1fd2ec8bd4f5",
+    "url": "/airbnb-page/static/js/1.08e9374f.chunk.js"
+  },
+  {
+    "revision": "2f6fed1e8c2d99a176ae",
+    "url": "/airbnb-page/static/js/runtime~main.2f6fed1e.js"
+  },
+  {
+    "revision": "cf1833bf7e4b96bb331b8ae933a0dba3",
+    "url": "/airbnb-page/static/media/hero-img.cf1833bf.png"
+  },
+  {
+    "revision": "9ad342d5e886f08b24176bf4d2e7e7cd",
+    "url": "/airbnb-page/static/media/katie.9ad342d5.png"
+  },
+  {
+    "revision": "1adcf2c3c91c605cdfda5688af66df49",
+    "url": "/airbnb-page/static/media/wedding-photography.1adcf2c3.png"
+  },
+  {
+    "revision": "765eb5280a41d2f86af9a60e57c12d99",
+    "url": "/airbnb-page/index.html"
+  }
+];
